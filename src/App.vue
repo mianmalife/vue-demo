4f489dd3e88686d88a4d80aa4d4e8d5ae068d7b3
zhangkx <zhangkx@jifenn.com>
2020-02-19 21:25:47 +0800

    add vuex

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <!-- <router-link to="/detail/1">Home</router-link>|
-      <router-link to="/about">About</router-link>-->
+      <router-link to="/detail/1">Home</router-link>|
+      <router-link to="/about">About</router-link>
 
-      <router-link to="#/">Home</router-link>|
-      <router-link to="#/about">About</router-link>
+      <!-- <router-link to="#/">Home</router-link>|
+      <router-link to="#/about">About</router-link> -->
       <router-view />
     </div>
 
